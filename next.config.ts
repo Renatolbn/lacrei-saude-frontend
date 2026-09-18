2b06fca0153a08cc22eff833f47fc20a3290fe3a
Renatolbn <renatolbn@gmail.com>
2026-09-18 20:06:13 -0300

    chore: configura dependencias do projeto

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
 
-module.exports = {
   compiler: {
     styledComponents: true,
   },
-}
+};
+
+
 
 export default nextConfig;
